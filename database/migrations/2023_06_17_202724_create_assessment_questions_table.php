fb67dbe6feaf3ed82e1a725b1ade5e278a6f3bab
felix ayange <felixdecoder2020@gmail.com>
2023-06-17 22:04:39 +0100

    add models,migrations and relationship for assessements

---
+++ database/migrations/2023_06_17_202724_create_assessment_questions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessment_questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assessment_condition_id')->constrained('assessment_conditions');
+            $table->text('question');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessment_questions');
+    }
+};
